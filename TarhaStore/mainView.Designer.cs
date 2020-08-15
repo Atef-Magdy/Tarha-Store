8960fa981f05a9a499d2a2f9fd9cc26164723f82
Atef Magdy <atefmagdy12@gmail.com>
2020-08-15 19:40:32 +0200

    Skelton files and auto suggestion function added

---
+++ TarhaStore/mainView.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace TarhaStore
+{
+    partial class mainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainView));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.storeDetails = new System.Windows.Forms.Button();
+            this.admin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pill = new System.Windows.Forms.DataGridView();
+            this.itemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tarhaDBDataSet = new TarhaStore.TarhaDBDataSet();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.discount = new System.Windows.Forms.TextBox();
+            this.total = new System.Windows.Forms.TextBox();
+            this.btnDiscount = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.add = new System.Windows.Forms.Button();
+            this.quantityBox = new System.Windows.Forms.TextBox();
+            this.print = new System.Windows.Forms.Button();
+            this.search = new System.Windows.Forms.TextBox();
+            this.itemsTableAdapter = new TarhaStore.TarhaDBDataSetTableAdapters.itemsTableAdapter();
+            this.quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tarhaDBDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // storeDetails
+            // 
+            resources.ApplyResources(this.storeDetails, "storeDetails");
+            this.storeDetails.Name = "storeDetails";
+            this.storeDetails.UseVisualStyleBackColor = true;
+            this.storeDetails.Click += new System.EventHandler(this.storeDetails_Click);
+            // 
+            // admin
+            // 
+            resources.ApplyResources(this.admin, "admin");
+            this.admin.Name = "admin";
+            this.admin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // pill
+            // 
+            this.pill.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arabic Typesetting", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pill.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.pill.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pill.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.quantity,
+            this.price,
+            this.name});
+            resources.ApplyResources(this.pill, "pill");
+            this.pill.Name = "pill";
+            this.pill.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // itemsBindingSource
+            // 
+            this.itemsBindingSource.DataMember = "items";
+            this.itemsBindingSource.DataSource = this.tarhaDBDataSet;
+            // 
+            // tarhaDBDataSet
+            // 
+            this.tarhaDBDataSet.DataSetName = "TarhaDBDataSet";
+            this.tarhaDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // discount
+            // 
+            resources.ApplyResources(this.discount, "discount");
+            this.discount.Name = "discount";
+            // 
+            // total
+            // 
+            resources.ApplyResources(this.total, "total");
+            this.total.Name = "total";
+            this.total.ReadOnly = true;
+            // 
+            // btnDiscount
+            // 
+            resources.ApplyResources(this.btnDiscount, "btnDiscount");
+            this.btnDiscount.Name = "btnDiscount";
+            this.btnDiscount.UseVisualStyleBackColor = true;
+            this.btnDiscount.Click += new System.EventHandler(this.btnDiscount_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // add
+            // 
+            resources.ApplyResources(this.add, "add");
+            this.add.Name = "add";
+            this.add.UseVisualStyleBackColor = true;
+            // 
+            // quantityBox
+            // 
+            resources.ApplyResources(this.quantityBox, "quantityBox");
+            this.quantityBox.Name = "quantityBox";
+            // 
+            // print
+            // 
+            resources.ApplyResources(this.print, "print");
+            this.print.Name = "print";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // search
+            // 
+            resources.ApplyResources(this.search, "search");
+            this.search.Name = "search";
+            // 
+            // itemsTableAdapter
+            // 
+            this.itemsTableAdapter.ClearBeforeFill = true;
+            // 
+            // quantity
+            // 
+            resources.ApplyResources(this.quantity, "quantity");
+            this.quantity.Name = "quantity";
+            this.quantity.ReadOnly = true;
+            // 
+            // price
+            // 
+            resources.ApplyResources(this.price, "price");
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
+            // 
+            // name
+            // 
+            resources.ApplyResources(this.name, "name");
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // mainView
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.quantityBox);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDiscount);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.discount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pill);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.admin);
+            this.Controls.Add(this.storeDetails);
+            this.Name = "mainView";
+            this.Load += new System.EventHandler(this.form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tarhaDBDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button storeDetails;
+        private System.Windows.Forms.Button admin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView pill;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox discount;
+        private System.Windows.Forms.TextBox total;
+        private System.Windows.Forms.Button btnDiscount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.TextBox quantityBox;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.TextBox search;
+        private TarhaDBDataSet tarhaDBDataSet;
+        private System.Windows.Forms.BindingSource itemsBindingSource;
+        private TarhaDBDataSetTableAdapters.itemsTableAdapter itemsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+    }
+}
+
---
+++ TarhaStore/mainView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TarhaStore
+{
+    public partial class mainView : Form
+    {
+        public mainView()
+        {
+            InitializeComponent();
+        }
+
+        private void printPreviewDialog1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnDiscount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+
+            connetionString = "Data Source=DESKTOP-MKVLAC4\\SQLEXPRESS; Initial Catalog=TarhaDB;Integrated Security=true;";
+            string sql = "INSERT INTO [TarhaDB].[dbo].[items] (name , price , quantity) VALUES('harer' , 20 , 70)";
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! " + ex);
+                
+            }
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+
+            connetionString = "Data Source=DESKTOP-MKVLAC4\\SQLEXPRESS; Initial Catalog=TarhaDB;Integrated Security=true;";
+            string sql = "SELECT name from [TarhaDB].[dbo].[items]";
+            connection = new SqlConnection(connetionString);
+            command = new SqlCommand(sql, connection);
+            connection.Open();
+            
+            SqlDataReader reader = command.ExecuteReader();
+            AutoCompleteStringCollection autoText = new AutoCompleteStringCollection();
+
+            while(reader.Read())
+            {
+                autoText.Add(reader.GetString(0));
+            }
+            search.AutoCompleteMode = AutoCompleteMode.Suggest;
+            search.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            search.AutoCompleteCustomSource = autoText;
+            connection.Close();
+
+        }
+
+        private void storeDetails_Click(object sender, EventArgs e)
+        {
+            storeDetails form2 = new storeDetails();
+            form2.Show();
+            this.Hide();
+        }
+    }
+}
